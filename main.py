b4daaf5c31d107b394986981839e790be31be6f6
Vishal <56838499+vishal2904@users.noreply.github.com>
2022-09-10 22:11:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ conn = sql.connect(host='localhost', user='root', passwd='1234', database='bank'
 cur = conn.cursor()
 # cur.execute('create table user_table(username varchar(25) primary key,passwrd varchar(25) not null )')
 print('=========================WELCOME TO STARK BANK============================================================')
-importdatetime as dt
+import datetime as dt
 print(dt.datetime.now())
 print('1.REGISTER')
 print()
