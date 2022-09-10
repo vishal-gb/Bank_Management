9ca8ec00e827fbc920373242203b56f66f54c5f0
Vishal <56838499+vishal2904@users.noreply.github.com>
2022-09-10 22:10:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #MAIN.PY
 
-importmysql.connector as sql
+import mysql.connector as sql
 conn = sql.connect(host='localhost', user='root', passwd='1234', database='bank')
 cur = conn.cursor()
 # cur.execute('create table user_table(username varchar(25) primary key,passwrd varchar(25) not null )')
